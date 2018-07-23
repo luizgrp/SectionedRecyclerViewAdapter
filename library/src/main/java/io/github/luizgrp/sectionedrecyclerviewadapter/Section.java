5c31948c8ec820456d45ca16280fae84ef2934bb
Gustavo Pagani <luizgrp@gmail.com>
2018-07-23 22:23:36 +0100

    Update gradle, build tools and libraries

--- library/src/main/java/io/github/luizgrp/sectionedrecyclerviewadapter/Section.java
+++ library/src/main/java/io/github/luizgrp/sectionedrecyclerviewadapter/Section.java
@@ -22,8 +22,8 @@ public abstract class Section {
 
     private boolean visible = true;
 
-    private boolean hasHeader = false;
-    private boolean hasFooter = false;
+    private boolean hasHeader;
+    private boolean hasFooter;
 
     @LayoutRes
     private final Integer itemResourceId;
@@ -348,7 +348,7 @@ public abstract class Section {
      * @param parent The parent view. Note that there is no need to attach the new view.
      * @return View for an Item of this Section.
      */
-    public View getItemView(ViewGroup parent) {
+    public View getItemView(@SuppressWarnings("unused") ViewGroup parent) {
         throw new UnsupportedOperationException(
                 "You need to implement getItemView() if you set itemViewWillBeProvided");
     }
