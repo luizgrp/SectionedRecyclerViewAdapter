73766aac05b77e41fa2ed7e4afeadbb8e14c6b82
Gustavo Pagani <luizgrp@gmail.com>
2019-05-20 17:36:30 +0100

    Improve navigation with navigation component

--- app/src/main/java/io/github/luizgrp/sectionedrecyclerviewadapter/demo/HomeActivity.java
+++ app/src/main/java/io/github/luizgrp/sectionedrecyclerviewadapter/demo/HomeActivity.java
@@ -1,98 +1,76 @@
 package io.github.luizgrp.sectionedrecyclerviewadapter.demo;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
+
 import com.google.android.material.navigation.NavigationView;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.ActionBarDrawerToggle;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.view.MenuItem;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
 
-public class HomeActivity extends AppCompatActivity
-    implements NavigationView.OnNavigationItemSelectedListener {
+import static androidx.navigation.Navigation.findNavController;
+import static androidx.navigation.ui.NavigationUI.navigateUp;
+import static androidx.navigation.ui.NavigationUI.setupActionBarWithNavController;
+import static androidx.navigation.ui.NavigationUI.setupWithNavController;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        final Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        bindViews();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        setUpToolbar();
+
+        setUpNavigation();
 
         if (savedInstanceState == null) {
-
-            HomeFragment homeFragment = new HomeFragment();
-
-            getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_container, homeFragment).commit();
-
-            drawer.openDrawer(GravityCompat.START);
+            drawerLayout.openDrawer(GravityCompat.START);
         }
+    }
+
+    private void bindViews() {
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navController = findNavController(this, R.id.nav_host_fragment);
+        navigationView = findViewById(R.id.nav_view);
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar(toolbar);
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    private void setUpNavigation() {
+        setupActionBarWithNavController(this, navController, drawerLayout);
+        setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navigateUp(navController, drawerLayout);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.nav_example1:
-                replaceFragment(new Example1Fragment());
-                break;
-            case R.id.nav_example2:
-                replaceFragment(new Example2Fragment());
-                break;
-            case R.id.nav_example3:
-                replaceFragment(new Example3Fragment());
-                break;
-            case R.id.nav_example4:
-                replaceFragment(new Example4Fragment());
-                break;
-            case R.id.nav_example5:
-                replaceFragment(new Example5Fragment());
-                break;
-            case R.id.nav_example6:
-                replaceFragment(new Example6Fragment());
-                break;
-            case R.id.nav_example7:
-                replaceFragment(new Example7Fragment());
-                break;
-            case R.id.nav_example8:
-                replaceFragment(new Example8Fragment());
-                break;
-        }
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    private void replaceFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-        transaction.replace(R.id.fragment_container, fragment);
-
-        transaction.commit();
     }
 }
